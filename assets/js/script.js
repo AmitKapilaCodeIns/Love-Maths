bc1ad3508d239110e3e814386091c73ee7baa599
AmitKapilaCodeIns <amitkapila@hotmail.com>
2025-04-03 08:58:25 +0100

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,15 +30,15 @@ function runGame(gameType) {
         case "addition":
             displayAdditionQuestion(op1, op2);
             break;
-        case "subtract":
-            displaySubtractQuestion(op1, op2);
-            break;
-        case "multiply":
-            displayMultiplyQuestion(op1, op2);
-            break;
-        case "division":
-            displayDivisionQuestion(op1, op2);
-            break;
+        // case "subtract":
+        //     displaySubtractQuestion(op1, op2);
+        //     break;
+        // case "multiply":
+        //     displayMultiplyQuestion(op1, op2);
+        //     break;
+        // case "division":
+        //     displayDivisionQuestion(op1, op2);
+        //     break;
         default:
             alert(`Invalid game type: ${gameType}`);
             throw `Invalid game type: ${gameType}. Aborting!`;
@@ -47,12 +47,34 @@ function runGame(gameType) {
 
 function checkAnswer() {}
 
-function calculateCorrectAnswer() {}
+/**
+ * Gets the operands and the operator from the DOM
+ * and returns the correct answer.
+ */
+function calculateCorrectAnswer() {
+    const operand1 = parseInt(document.getElementById("operand1").innerText);
+    const operand2 = parseInt(document.getElementById("operand2").innerText);
+    const operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    } else if (operator === "*") {
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    } else {
+        alert(`Unrecognized operator: ${operator}`);
+        throw `Unrecognized operator: ${operator}`;
+    }
+}
 
 function incrementScore() {}
 
 function incrementWrongAnswer() {}
 
+// https://www.youtube.com/watch?v=BVBtbYh3YSU textContent VS innerText
 function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;

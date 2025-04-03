7fdaa053f0244ec4952f62d65ede99da1096a5dd
AmitKapilaCodeIns <amitkapila@hotmail.com>
2025-04-03 19:26:36 +0100

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,9 +31,9 @@ function runGame(gameType) {
         case "addition":
             displayAdditionQuestion(op1, op2);
             break;
-        // case "subtract":
-        //     displaySubtractQuestion(op1, op2);
-        //     break;
+        case "subtract":
+            displaySubtractQuestion(op1, op2);
+            break;
         case "multiply":
             displayMultiplyQuestion(op1, op2);
             break;
@@ -79,11 +79,9 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    }
-    // else if (operator === "-") {
-    //     return [operand1 - operand2, "subtract"];
-    // }
-    else if (operator === "x") {
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     }
     // else if (operator === "/") {
@@ -120,7 +118,13 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {}
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent =
+        operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent =
+        operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
+}
 
 function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
